9434d5f04d17bec38022d6bc6bec8bcec4fbec0e
milda.dvorak <milda.dvorak@gmail.com>
2021-09-16 01:12:35 +0200

    add new field to model

--- models/models.py
+++ models/models.py
@@ -2,10 +2,19 @@
 
 from odoo import models, fields, api
 
-# class odoo_travel_epenses(models.Model):
+class hr_expense(models.Model):
+    _inherit = 'hr_expense'
 #     _name = 'odoo_travel_epenses.odoo_travel_epenses'
 
-#     name = fields.Char()
+
+    is_travel = fields.Boolean()
+    date_from = fields.Datetime()
+    date_from = fields.Datetime()
+    free_food_num = fields.Integer()
+    total_travel = fields.Float( digits=(18, 6))
+    first_day_hours = fields.Float( digits=(18, 6))
+    last_day_hours = fields.Float( digits=(18, 6))
+    full_day_num = fields.Integer()         #duration_days
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
@@ -13,3 +22,6 @@ from odoo import models, fields, api
 #     @api.depends('value')
 #     def _value_pc(self):
 #         self.value2 = float(self.value) / 100
+
+
+#     _inherit = ''
