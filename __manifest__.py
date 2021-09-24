5f8ade5efb46482b90a2ee921510790cf3950a62
milda.dvorak <milda.dvorak@gmail.com>
2021-09-24 20:48:41 +0200

    move to test docker add view

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "odoo_travel_epenses",
+    'name': "odoo_travel_expenses",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -10,17 +10,17 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Milda",
+    'website': "http://www.milda.top",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'HR',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'hr_expense'],
 
     # always loaded
     'data': [
--- models/models.py
+++ models/models.py
@@ -3,18 +3,25 @@
 from odoo import models, fields, api
 
 class hr_expense(models.Model):
-    _inherit = 'hr_expense'
+    _inherit = 'hr.expense'
 #     _name = 'odoo_travel_epenses.odoo_travel_epenses'
 
 
     is_travel = fields.Boolean()
     date_from = fields.Datetime()
-    date_from = fields.Datetime()
-    free_food_num = fields.Integer()
+    date_to = fields.Datetime()
+    free_food_num = fields.Float( digits=(18, 6))
     total_travel = fields.Float( digits=(18, 6))
     first_day_hours = fields.Float( digits=(18, 6))
     last_day_hours = fields.Float( digits=(18, 6))
-    full_day_num = fields.Integer()         #duration_days
+    full_day_num = fields.Float( digits=(18, 6))
+    compute_time_diff = fields.Float( digits=(18, 6))
+    compute_day_diff = fields.Float( digits=(18, 6))
+    compute_remain_hours = fields.Float( digits=(18, 6))
+    compute_travel_base_price = fields.Float( digits=(18, 6))
+
+
+             #duration_days
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
