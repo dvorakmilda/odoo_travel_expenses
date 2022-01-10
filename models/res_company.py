6b7593b307fb03d093dcbbc08b94b2e82abbc966
milda.dvorak <milda.dvorak@gmail.com>
2022-01-10 20:55:32 +0100

    better code

--- models/res_company.py
+++ models/res_company.py
@@ -6,8 +6,8 @@ from odoo import models, fields, api
 class res_company(models.Model):
     _inherit = 'res.company'
 
-    exp_min = fields.Float(string='', digits=(18, 6))
-    exp_mid = fields.Float(string='', digits=(18, 6))
-    exp_max = fields.Float(string='', digits=(18, 6))
+    exp_min = fields.Float(string='', digits=(18, 6), default=108)
+    exp_mid = fields.Float(string='', digits=(18, 6), default=167)
+    exp_max = fields.Float(string='', digits=(18, 6), default=259)
 
 
